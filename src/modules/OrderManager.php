a43e3d1e8fbc8bee00a2d01fa2d513175a851e71
atomlab <aldrinthomaslab@gmail.com>
2019-04-18 19:22:54 +0800

    Creates OrderManager class

---
+++ src/modules/OrderManager.php
@@ -0,0 +1,16 @@
+<?php
+include_once('./../interfaces/ProductSoldInterface.php');
+include_once('./../interfaces/OrderProcessorInterface.php');
+
+class OrderManager implements OrderProcessorInterface, ProductSoldInterface
+{
+  public function processFromJson(string $filePath): void
+  {
+
+  }
+
+  public function getSoldTotal(int $productId): int
+  {
+    
+  }
+}
