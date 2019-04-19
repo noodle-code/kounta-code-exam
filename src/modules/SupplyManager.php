6513e44ee92a6b137dedbf38f5894f670edc498f
atomlab <aldrinthomaslab@gmail.com>
2019-04-19 13:36:32 +0800

    Fix expected return type for setPurchaseFor method

--- src/modules/SupplyManager.php
+++ src/modules/SupplyManager.php
@@ -20,19 +20,35 @@ class SupplyManager implements ProductPurchasedInterface
 
   }
 
-  public function setPurchaseFor (int $itemId, int $orderDay): void
+  public function setPurchaseFor (int $itemId, int $orderDay): boolean
   {
     if (empty($this->pendingPurchases[$itemId]))
     {
       $this->pendingPurchases[$itemId] = [
         "purchase_day" => $orderDay,
         "quantity"     => 20,
-        "receive_day"  => $orderDay + 2;
+        "receive_day"  => $orderDay + 2
       ];
 
       return true;
     }
 
     return false;
+  }
+
+  public function receivePurchases (int $dayId): array
+  {
+    $receive = [];
+
+    foreach($this->pendingPurchases as $item => $order)
+    {
+      if ($order['receive_day'] === $dayId)
+      {
+        $receive[$item] = $order['quantity'];
+        $receivedPurchases[$item][] = $order;
+      }
+    }
+
+    return $receive;
   }
 }
