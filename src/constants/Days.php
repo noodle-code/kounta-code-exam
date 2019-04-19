8747a97115cc7ce411e056308de8b7900b01ae53
atomlab <aldrinthomaslab@gmail.com>
2019-04-19 12:01:25 +0800

    Creates Days constants class

---
+++ src/constants/Days.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kounta\Constants;
+
+class Days
+{
+  public const MONDAY = 1;
+  public const TUESDAY = 2;
+  public const WEDNESDAY = 3;
+  public const THURSDAY = 4;
+  public const FRIDAY = 5;
+  public const SATURDAY = 6;
+  public const SUNDAY = 7;
+}
