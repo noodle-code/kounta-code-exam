b5f1d96cb357061de7d234c37bdbc77168cd916d
atomlab <aldrinthomaslab@gmail.com>
2019-04-19 12:41:00 +0800

    Creates tests for SupplyManager setPurchase method

--- tests/SupplyManagerTest.php
+++ tests/SupplyManagerTest.php
@@ -2,7 +2,43 @@
 
 use PHPUnit\Framework\TestCase;
 
+use Kounta\Modules\SupplyManager;
+use Kounta\Constants\Products;
+use Kounta\Constants\Days;
+
 class SupplyManagerTest extends TestCaese
 {
-  
+  private $supplyManager;
+
+  public function setUp()
+  {
+    if (empty($supplyManager))
+    {
+      $this->supplyManager = new SupplyManager();
+    }
+  }
+
+  public function testShouldReceiveOrderOnWednesdayWhenAMondayPurchaseForBrowniesIsDone ()
+  {
+    $expected = [
+      "purchase_day"  => Days::MONDAY,
+      "quantity"      => 20,
+      "receive_day"   => Days::WEDNESDAY
+    ];
+
+    $this->assertTrue($this->supplyManager->setPurchaseFor(Products::BROWNIE, Days::MONDAY));
+    $this->assertEquals($expected, $this->supplyManager->getPurchaseFor(Products::BROWNIE));
+  }
+
+  public function testShouldRejectAnotherPurchaseOfBrowniesOnTueday ()
+  {
+    $expected = [
+      "purchase_day"  => Days::MONDAY,
+      "quantity"      => 20,
+      "receive_day"   => Days::WEDNESDAY
+    ];
+
+    $this->assertFalse($this->supplyManager->setPurchaseFor(Products::BROWNIE, Days::TUESDAY));
+    $this->assertEquals($expected, $this->supplyManager->getPurchaseFor(Products::BROWNIE));
+  }
 }
