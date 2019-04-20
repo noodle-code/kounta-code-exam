4315c41e4b7821d57e39e501b5cae69eea4b5807
atomlab <aldrinthomaslab@gmail.com>
2019-04-21 03:58:03 +0800

    Creates tests for WeekIterator Class

---
+++ tests/WeekIterationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Kounta\Modules\WeekIterator;
+use Kounta\Constants\Days;
+
+class WeekIterationTest extends TestCase;
+{
+  public function testShouldRunIteratorFor4DaysAndResultDayShouldBeWednesday ()
+  {
+
+  }
+
+  public function testShouldRunItearatorFor10DaysAndResultDayShouldBeWednesday ()
+  {
+
+  }
+
+  public function testShouldRunIteratorFromTuedayFor5DaysAndResultDayShouldBeSunday ()
+  {
+
+  }
+}
