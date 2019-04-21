e8054c2488f3e85edf10c633d5bb41da3cd5ba79
atomlab <aldrinthomaslab@gmail.com>
2019-04-21 11:14:20 +0800

    Defines test cases for WeekIteration class

--- tests/WeekIterationTest.php
+++ tests/WeekIterationTest.php
@@ -5,20 +5,41 @@ use PHPUnit\Framework\TestCase;
 use Kounta\Modules\WeekIterator;
 use Kounta\Constants\Days;
 
-class WeekIterationTest extends TestCase;
+class WeekIterationTest extends TestCase
 {
   public function testShouldRunIteratorFor4DaysAndResultDayShouldBeWednesday ()
   {
+    $iterator  = new WeekIterator();
+    $resultDay = 0;
 
+    $iterator->iterateWeek( function ($currentDay) use (&$resultDay) {
+      $resultDay = $currentDay;
+    }, 4);
+
+    $this->assertEquals(Days::WEDNESDAY, $resultDay);
   }
 
-  public function testShouldRunItearatorFor10DaysAndResultDayShouldBeWednesday ()
+  public function testShouldRunIteratorFor10DaysAndResultDayShouldBeTuesday ()
   {
+    $iterator  = new WeekIterator();
+    $resultDay = 0;
 
+    $iterator->iterateWeek( function ($currentDay) use (&$resultDay) {
+      $resultDay = $currentDay;
+    }, 10);
+
+    $this->assertEquals(Days::TUESDAY, $resultDay);
   }
 
-  public function testShouldRunIteratorFromTuedayFor5DaysAndResultDayShouldBeSunday ()
+  public function testShouldRunIteratorFromTuedayFor5DaysAndResultDayShouldBeSaturday ()
   {
+    $iterator  = new WeekIterator(Days::TUESDAY);
+    $resultDay = 0;
 
+    $iterator->iterateWeek( function ($currentDay) use (&$resultDay) {
+      $resultDay = $currentDay;
+    }, 5);
+
+    $this->assertEquals(Days::SATURDAY, $resultDay);
   }
 }
